85489ff09406bd55bd578acadc564e8fda514d78
Joonas <joonas.yoon@gmail.com>
2018-07-30 09:50:38 +0000

    remove socket.io connection

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/collect',
   sendUploadToGCS, 
   extractAudioToWav,
   function (req, res) {
-    console.log(req.file);
+    console.log(req.body);
 
     syncRecognize(
       req.file.convertedPath,
@@ -61,41 +61,6 @@ app.post('/collect',
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
 });
-
-// Socket connection
-// Creates new HTTP server for socket
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(8080, function(){
-  console.log('Socket server listening on : 8080');
-});
-
-let
-    sequenceNumberByClient = new Map();
-
-// event fired every time a new client connects:
-io.on('connection', (socket) => {
-    console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number
-    sequenceNumberByClient.set(socket, 1);
-
-    // when socket disconnects, remove it from the list:
-    socket.on('disconnect', () => {
-        sequenceNumberByClient.delete(socket);
-        console.info(`Client gone [id=${socket.id}]`);
-    });
-});
-
-// sends each client its current sequence number
-setInterval(() => {
-    // console.log('emit function!');
-    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-        client.emit('counter', sequenceNumber);
-        sequenceNumberByClient.set(client, sequenceNumber + 1);
-    }
-}, 500 * 1000);
-
 
 function syncRecognize(filename, encoding, sampleRateHertz, languageCode, callback) {
   // [START speech_sync_recognize]
